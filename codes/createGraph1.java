9d4355bf0846da585571dfd7aaebf0b62e3673b2
arpitjaingwl7 <arpitjaingwl7@gmail.com>
2024-09-15 16:30:34 +0530

    Graph Implentation Generic Type

--- codes/createGraph1.java
+++ codes/createGraph1.java
@@ -5,96 +5,82 @@ import java.util.*;
 public class createGraph1 {
 
   public static void main(String[] args) {
- 
-    Graph g1=createUndirectedGraph();
+    
+    Graph<Integer> g1 = createUndirectedGraph();
     g1.printGraph();
   }
 
-  // Fuction That create a undirected Graph
-  public static Graph createUndirectedGraph() {
+  // Function to create an undirected graph
+  public static Graph<Integer> createUndirectedGraph() {
 
     Scanner sc = new Scanner(System.in);
 
     System.out.println("Enter Number of Nodes ");
     int n = sc.nextInt();
-    Graph g1 = new Graph(n);
+    Graph<Integer> g1 = new Graph<>();
 
-    System.out.println("Enter Number of Endges");
+    System.out.println("Enter Number of Edges");
     int e = sc.nextInt();
 
     for (int i = 1; i <= e; i++) {
-      System.out.println("enter the " + i + "numberth edge");
+      System.out.println("Enter the " + i + "th edge");
       int u = sc.nextInt();
       int v = sc.nextInt();
       g1.addEdge(u, v, false);
-
     }
+
     sc.close();
-
     return g1;
-
   }
 
-   // Fuction That create a Directed Graph
-
-   public static Graph createDirectedGraph() {
+  // Function to create a directed graph
+  public static Graph<Integer> createDirectedGraph() {
 
     Scanner sc = new Scanner(System.in);
 
     System.out.println("Enter Number of Nodes ");
     int n = sc.nextInt();
-    Graph g1 = new Graph(n);
+    Graph<Integer> g1 = new Graph<>();
 
-    System.out.println("Enter Number of Endges");
+    System.out.println("Enter Number of Edges");
     int e = sc.nextInt();
 
     for (int i = 1; i <= e; i++) {
-      System.out.println("enter the " + i + "numberth edge");
+      System.out.println("Enter the " + i + "th edge");
       int u = sc.nextInt();
       int v = sc.nextInt();
       g1.addEdge(u, v, true);
-
     }
+
     sc.close();
-
     return g1;
-
   }
-
-
 }
 
-class Graph {
-  HashMap<Integer, List<Integer>> adj;
+class Graph<T> {
+  private Map<T, List<T>> adj;
 
-  Graph(int nodes) {
+  public Graph() {
     adj = new HashMap<>();
-    for (int i = 0; i < nodes; i++) {
-      adj.put(i, new ArrayList<>());
-    }
-
   }
 
-  public void addEdge(int u, int v, boolean dir) {
-    // dir=true->directed
-    // dir=false->non directed
-    this.adj.get(u).add(v);
+  public void addEdge(T u, T v, boolean dir) {
+    adj.putIfAbsent(u, new ArrayList<>());
+    adj.get(u).add(v);
+
     if (!dir) {
-      this.adj.get(v).add(u);
+      adj.putIfAbsent(v, new ArrayList<>());
+      adj.get(v).add(u);
     }
   }
 
   public void printGraph() {
+    System.out.println("Your Graph Is:");
 
-    System.out.println("Your Graph Is-");
-
-    for (int i = 0; i < adj.size(); i++) {
-      for (int j : adj.get(i)) {
-
-        System.out.println(i + "-->" + j);
+    for (T node : adj.keySet()) {
+      for (T neighbor : adj.get(node)) {
+        System.out.println(node + " --> " + neighbor);
       }
     }
-
   }
-
 }
