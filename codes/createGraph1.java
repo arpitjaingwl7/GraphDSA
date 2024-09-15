be9e1b32e7a25e9c8a455c358f5c82a7aad3c4db
arpitjaingwl7 <arpitjaingwl7@gmail.com>
2024-09-15 17:26:54 +0530

    funtion Generic fixed

--- codes/createGraph1.java
+++ codes/createGraph1.java
@@ -5,58 +5,61 @@ import java.util.*;
 public class createGraph1 {
 
   public static void main(String[] args) {
-    
-    Graph<Integer> g1 = createUndirectedGraph();
-    g1.printGraph();
+    Scanner sc = new Scanner(System.in);
+
+    // Create an undirected graph
+    Graph<Character> undirectedGraph = createUndirectedGraph(sc);
+    undirectedGraph.printGraph();
+
+    // // Create a directed graph
+    // Graph<Integer> directedGraph = createDirectedGraph(sc);
+    // directedGraph.printGraph();
+
+    sc.close();
   }
 
   // Function to create an undirected graph
-  public static Graph<Integer> createUndirectedGraph() {
+  public static <T> Graph<T> createUndirectedGraph(Scanner sc) {
+    System.out.println("Enter Number of Nodes: ");
+    int n = sc.nextInt(); // Not used in logic, but can be used for validation
 
-    Scanner sc = new Scanner(System.in);
+    Graph<T> graph = new Graph<>();
 
-    System.out.println("Enter Number of Nodes ");
-    int n = sc.nextInt();
-    Graph<Integer> g1 = new Graph<>();
-
-    System.out.println("Enter Number of Edges");
+    System.out.println("Enter Number of Edges: ");
     int e = sc.nextInt();
 
     for (int i = 1; i <= e; i++) {
-      System.out.println("Enter the " + i + "th edge");
-      int u = sc.nextInt();
-      int v = sc.nextInt();
-      g1.addEdge(u, v, false);
+      System.out.println("Enter the " + i + "th edge (u and v):");
+      T u = (T) sc.next(); // Reading edges as generic type (assuming T is Integer or String)
+      T v = (T) sc.next();
+      graph.addEdge(u, v, false);
     }
 
-    sc.close();
-    return g1;
+    return graph;
   }
 
   // Function to create a directed graph
-  public static Graph<Integer> createDirectedGraph() {
+  public static <T> Graph<T> createDirectedGraph(Scanner sc) {
+    System.out.println("Enter Number of Nodes: ");
+    int n = sc.nextInt(); // Not used in logic, but can be used for validation
 
-    Scanner sc = new Scanner(System.in);
+    Graph<T> graph = new Graph<>();
 
-    System.out.println("Enter Number of Nodes ");
-    int n = sc.nextInt();
-    Graph<Integer> g1 = new Graph<>();
-
-    System.out.println("Enter Number of Edges");
+    System.out.println("Enter Number of Edges: ");
     int e = sc.nextInt();
 
     for (int i = 1; i <= e; i++) {
-      System.out.println("Enter the " + i + "th edge");
-      int u = sc.nextInt();
-      int v = sc.nextInt();
-      g1.addEdge(u, v, true);
+      System.out.println("Enter the " + i + "th edge (u and v):");
+      T u = (T) sc.next();
+      T v = (T) sc.next();
+      graph.addEdge(u, v, true);
     }
 
-    sc.close();
-    return g1;
+    return graph;
   }
 }
 
+// Generic Graph class
 class Graph<T> {
   private Map<T, List<T>> adj;
 
@@ -68,19 +71,21 @@ class Graph<T> {
     adj.putIfAbsent(u, new ArrayList<>());
     adj.get(u).add(v);
 
-    if (!dir) {
+    if (!dir) { // For undirected graph
       adj.putIfAbsent(v, new ArrayList<>());
       adj.get(v).add(u);
     }
   }
 
   public void printGraph() {
-    System.out.println("Your Graph Is:");
+    System.out.println("Your Graph:");
 
     for (T node : adj.keySet()) {
+      System.out.print(node + " --> " );
       for (T neighbor : adj.get(node)) {
-        System.out.println(node + " --> " + neighbor);
+        System.out.print( neighbor+", ");
       }
+      System.out.println(5);
     }
   }
 }
